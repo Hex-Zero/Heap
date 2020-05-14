ffd25c566578d2046eae5c1564cef9c14af5b5a5
Ace Alrich <hex0@live.com>
2020-05-14 15:37:41 +0100

    add a class attribute

--- heap.js
+++ heap.js
@@ -2,22 +2,20 @@
 const titleText = document.getElementById("title-text")
 titleText.innerHTML = "New Text"
 
-
-
-
 const generateRandomArray = (amount = 10, min = 0, max = 10) =>{
     const resultingArray = []
     let node
     for(let i = 0; i < amount; i++){
        node = document.createElement("div")
-        node.setAttribute("id","node" + i)
+        node.setAttribute("id","node-" + i)
+        node.setAttribute("class","nodes")
         node.appendChild(document.createTextNode(Math.floor(Math.random() * max)))
         resultingArray.push(node)
     }
     return resultingArray
 }
-generateRandomArray().forEach((value, index)=>{
-    console.log(value);
+generateRandomArray(5).forEach((value, index)=>{
+    titleText.appendChild(value)
 
 })
 
