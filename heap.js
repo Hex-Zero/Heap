b1dff922626dfa9d9517b7bf16226f14cee1e96b
Ace Alrich <hex0@live.com>
2020-05-14 15:19:43 +0100

    add id to created element

--- heap.js
+++ heap.js
@@ -3,6 +3,8 @@ const titleText = document.getElementById("title-text")
 titleText.innerHTML = "New Text"
 
 let node = document.createElement("div")
+node.setAttribute("id","node1")
 node.appendChild(document.createTextNode("More text"))
 
 titleText.appendChild(node)
+node.appendChild(node)
