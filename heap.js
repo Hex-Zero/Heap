1245e721ef8ebc221a15230e4e7998a50d725b48
Ace Alrich <hex0@live.com>
2020-05-14 15:50:16 +0100

    include both min and max

--- heap.js
+++ heap.js
@@ -1,21 +1,22 @@
+const titleText = document.getElementById("title-text");
+titleText.innerHTML = "New Text";
 
-const titleText = document.getElementById("title-text")
-titleText.innerHTML = "New Text"
-
-const generateRandomArray = (amount = 10, min = 0, max = 10) =>{
-    const resultingArray = []
-    let node
-    for(let i = 0; i < amount; i++){
-       node = document.createElement("div")
-        node.setAttribute("id","node-" + i)
-        node.setAttribute("class","nodes")
-        node.appendChild(document.createTextNode(Math.floor(Math.random() * ( max - min )) + min))
-        resultingArray.push(node)
-    }
-    return resultingArray
-}
-generateRandomArray(5).forEach((value, index)=>{
-    titleText.appendChild(value)
-
-})
-
+const generateRandomArray = (amount = 10, min = 0, max = 10) => {
+  const resultingArray = [];
+  let node;
+  for (let i = 0; i < amount; i++) {
+    node = document.createElement("div");
+    node.setAttribute("id", "node-" + i);
+    node.setAttribute("class", "nodes");
+    node.appendChild(
+      document.createTextNode(
+        Math.floor(Math.random() * (max - min - 1)) + min + 1
+      )
+    );
+    resultingArray.push(node);
+  }
+  return resultingArray;
+};
+generateRandomArray(5, 19, 12).forEach((value, index) => {
+  titleText.appendChild(value);
+});
