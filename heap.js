c388eb795b3988747c4eb3b24d22b6634aca496f
Ace Alrich <hex0@live.com>
2020-05-14 17:35:52 +0100

    everi parent had 2 children

--- heap.js
+++ heap.js
@@ -1,22 +1,32 @@
 const titleText = document.getElementById("title-text");
 titleText.innerHTML = "New Text";
 
-const generateRandomArray = (amount = 10, min = 0, max = 10) => {
+const generateRandomArray = (amount = 12, min = 0, max = 9) => {
   const resultingArray = [];
   let node;
+  let parentNode;
+  let isLeft = false;
   for (let i = 0; i < amount; i++) {
     node = document.createElement("div");
     node.setAttribute("id", "node-" + i);
     node.setAttribute("class", "nodes");
     node.appendChild(
-      document.createTextNode(
-        Math.floor(Math.random() * (max - min - 1)) + min + 1
-      )
+      //   document.createTextNode(Math.floor(Math.random() * (max - min + 1)) + min)
+      document.createTextNode(i)
     );
-    resultingArray.push(node);
+    if (i == 0) {
+      parentNode = titleText;
+    } else {
+      if (isLeft) {
+        parentNode = document.getElementById(`node-${i / 2 - 0.5}`);
+      } else {
+        parentNode = document.getElementById(`node-${i / 2 - 1}`);
+      }
+    }
+
+    isLeft = !isLeft;
+
+    parentNode.appendChild(node);
   }
-  return resultingArray;
 };
-generateRandomArray(5, 19, 12).forEach((value, index) => {
-  titleText.appendChild(value);
-});
+generateRandomArray(30);
