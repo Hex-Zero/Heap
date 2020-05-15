9abfd94db5ae7326589254dc0f3de9293385d322
Ace Alrich <hex0@live.com>
2020-05-15 23:45:09 +0100

    Drop svg use canvas

--- heap.js
+++ heap.js
@@ -1,8 +1,24 @@
 const titleText = document.getElementById("title-text");
 titleText.innerHTML = "New Text";
 let rect = titleText.getBoundingClientRect();
-console.log(rect);
-
+let bodyTag = document.getElementById("body");
+let line;
+let lineSvg;
+const drawLines = (span, parentNode) => {
+  lineSvg = document.createElement("svg");
+  line = document.createElement("circl");
+  lineSvg.setAttribute("width", "100%");
+  lineSvg.setAttribute("height", "100%");
+  line.setAttribute("cx", "10");
+  line.setAttribute("cy", "10");
+  line.setAttribute("r", "2");
+  // line.setAttribute("y2", "150");
+  line.setAttribute("fill", "black");
+  lineSvg.innerHTML = "<line/>";
+  bodyTag.appendChild(lineSvg);
+  console.log(lineSvg);
+};
+drawLines();
 const nodeArray = [];
 const generateRandomArray = (amount = 12, min = 0, max = 9) => {
   const resultingArray = [];
@@ -33,20 +49,16 @@ const generateRandomArray = (amount = 12, min = 0, max = 9) => {
         parentNode = document.getElementById(`node-${i / 2 - 1}`);
         node.classList.add("right");
       }
+      // drawLines(paragraph, parentNode);
     }
+
     // console.log(paragraph.getBoundingClientRect());
     // console.log(titleText.getBoundingClientRect());
     isLeft = !isLeft;
-    nodeArray.push(node);
+    nodeArray.push(paragraph);
 
     parentNode.appendChild(node);
   }
   return nodeArray;
 };
 generateRandomArray(16);
-
-const drawLines = () => {
-  nodeArray.forEach((item) => console.log(item.getBoundingClientRect()));
-};
-
-drawLines();
